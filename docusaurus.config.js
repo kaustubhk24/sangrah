73ecf8ee8f5c318550ef0adc8118f3bccf58c687
anushkakulkarni121-creator <anushka.kulkarni121@gmail.com>
2026-06-26 23:12:47 +0530

    pilot test #2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,8 @@ const config = {
   favicon: 'img/ico.png',
 
   // Set the production url of your site here
-  url: 'https://sangrah.justinclicks.com',
+  //url: 'https://sangrah.justinclicks.com',
+  url:'https://stage-sangrah.netlify.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
